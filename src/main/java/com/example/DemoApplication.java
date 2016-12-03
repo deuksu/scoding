7121b2d82c891010416bb8584b13b11e475aef83
11 <deuksu.bang@gmail.com>
2016-12-03 15:27:03 +0900

    11111

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 //@ComponentScan
 //@Configuration
-@SpringBootApplication
+@SpringBootApplication  
 public class DemoApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
